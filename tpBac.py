6314155a6d18ebe0216e34a17067bc11e100257a
marrev <julien.depeyris@laposte.net>
2024-10-08 11:59:53 +0200

    Version B
    Signed-off-by: marrev <julien.depeyris@laposte.net>

--- tpBac.py
+++ tpBac.py
@@ -12,30 +12,41 @@ import time
 
 #déclaration des sémaphores: valeur init, info pour le debug
 sem1 = infem.Semaphore(4,"Embarquement voiture")
-sem2 = infem.Semaphore(0,"Debarquement voiture")
+sem2 = infem.Semaphore(0,"Débarquement voiture")
 sem3 = infem.Semaphore(0,"Aller")
 sem4 = infem.Semaphore(0,"Retour")
+sem5 = infem.Semaphore(0,"Terminaison propre")
+sem6 = infem.Semaphore(1,"Thread Bateau")
+sem7 = infem.Semaphore(10,"Threads voiture")
+
 
 def ferryRun(self):
     """ fonction principale du bac. Les methodes principales sont:
         - self.traverser()
         - self.revenir()
     """
-    while True:
+    timeout = 100.
+    cond = infem.ou2Semaphore(sem5,sem6,timeout)
+    while cond == 2:
         self.log("En attente des voitures")
         for i in range(4):
-            sem3.acquire()
+            if infem.ou2Semaphore(sem5,sem3,timeout)==1:
+                return 0
         self.log("Eh ze bardi !")
         self.traverser()
         for i in range(4):
             sem2.release()
         for i in range(4):
-            sem4.acquire()
+            if infem.ou2Semaphore(sem5,sem4,timeout)==1:
+                return 0
         self.log("Eh za revient !")
         self.revenir()
         for i in range(4):
             sem1.release()
-            
+        sem6.release()
+        cond = infem.ou2Semaphore(sem5,sem6,timeout)
+
+    self.log("Fin de tâche")
 
 def carRun(self):
     """ fonction principale de chaque voiture. Les méthodes principales sont:
@@ -43,19 +54,31 @@ def carRun(self):
         - self.embarquer()
         - self.debarquer()
     """
-    while True:
+    timeout = 100.
+    cond = infem.ou2Semaphore(sem5,sem7,timeout)
+    while cond == 2:
         self.avancer()
         self.log("Devant l'eau!")
-        sem1.acquire()
-        self.embarquer()
-        sem3.release()
-        sem2.acquire()
-        self.debarquer()
-        sem4.release()
+        if infem.ou2Semaphore(sem5,sem1,timeout)==1:
+            return 0
+        else:
+            self.embarquer()
+            sem3.release()
+        if infem.ou2Semaphore(sem5,sem2,timeout)==1:
+            return 0
+        else:
+            self.debarquer()
+            sem4.release()
+            sem7.release()
+        cond = infem.ou2Semaphore(sem5,sem7,timeout)
+    
+    self.log("Fin de tâche")
 
 def terminateCalled(self):
     """ fonction appelée lors d'un clic sur le bouton de terminaison """
     print("L'application doit se terminer correctement")
+    for i in range(11):
+        sem5.release()
 
 #associe les fonctions aux methodes de classes (ne pas modifier)
 ferry.run = ferryRun
