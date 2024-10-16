2741abe75f9379c42f90fbd522ffee2e1839fe2b
hugodlrll <hugo.del49@gmail.com>
2024-10-16 09:47:18 +0200

    Version B terminée
    Signed-off-by: hugodlrll <hugo.del49@gmail.com>

--- tpBac.py
+++ tpBac.py
@@ -15,23 +15,38 @@ sem1 = infem.Semaphore(4,"Attente du chargement de 4 voitures")
 sem2 = infem.Semaphore(0, "Autorise la traversée")
 sem3 = infem.Semaphore(0, "Autoriser le débarquement")
 sem4 = infem.Semaphore(0, "Autoriser la traversée")
+sem5 = infem.Semaphore(0, "Terminaison")
+sem6 = infem.Semaphore(1, "Thread Bateau")
+sem7 = infem.Semaphore(10, "Thread Voiture")
+
 
 def ferryRun(self):
     """ fonction principale du bac. Les methodes principales sont:
         - self.traverser()
         - self.revenir()
     """
-    while True :
-        for i in range(4):
-            sem2.P()
-        self.traverser()
-        for i in range(4):
-            sem3.V()
-        for i in range(4):
-            sem4.P()
-        self.revenir()
-        for i in range(4):
-            sem1.V()
+    cond=infem.ou2Semaphore(sem5, sem6)
+    while cond == 2 :
+        i = 0
+        j = 0
+        while i < 4 and cond == 2:
+            cond = infem.ou2Semaphore(sem5, sem2)
+            i+=1
+        if cond == 2:
+            self.traverser()
+            self.log("Traversée effectuée")
+            for i in range(4):
+                sem3.V()
+        while j < 4 and cond == 2:
+            infem.ou2Semaphore(sem5, sem4)
+            j+=1
+        if cond == 2:
+            self.revenir()
+            self.log("Retour effectué")
+            for i in range(4):
+                sem1.V()
+            sem6.V()
+        cond=infem.ou2Semaphore(sem5, sem6)
 
 
     
@@ -42,20 +57,25 @@ def carRun(self):
         - self.embarquer()
         - self.debarquer()
     """
-    while True:
+    cond=infem.ou2Semaphore(sem5, sem7)
+    while cond == 2:
         self.avancer()
         self.log("devant l'eau!")
-        sem1.P()
-        self.embarquer()
-        self.log("voiture embarquée!")
-        sem2.V()
-        sem3.P()
-        self.debarquer()
-        sem4.V()
+        if infem.ou2Semaphore(sem5, sem1) == 2:
+            self.embarquer()
+            self.log("voiture embarquée!")
+            sem2.V()
+        if infem.ou2Semaphore(sem5, sem3) == 2:
+            self.debarquer()
+            sem4.V()
+            sem7.V()
+        cond=infem.ou2Semaphore(sem5, sem7)
 
 def terminateCalled(self):
     """ fonction appelée lors d'un clic sur le bouton de terminaison """
     print("L'application doit se terminer correctement")
+    for i in range (32):
+     sem5.V()
 
 #associe les fonctions aux methodes de classes (ne pas modifier)
 ferry.run = ferryRun
