6650676320d4534f673c852795413bda7f9fa5ae
marrev <julien.depeyris@laposte.net>
2024-10-16 08:21:14 +0200

    Essai Version C
    Signed-off-by: marrev <julien.depeyris@laposte.net>

--- tpBac.py
+++ tpBac.py
@@ -18,30 +18,44 @@ sem4 = infem.Semaphore(0,"Retour")
 sem5 = infem.Semaphore(0,"Terminaison propre")
 sem6 = infem.Semaphore(1,"Thread Bateau")
 sem7 = infem.Semaphore(10,"Threads voiture")
-
+sem8 = infem.Semaphore(0, "Sécu embarquement")
 
 def ferryRun(self):
     """ fonction principale du bac. Les methodes principales sont:
         - self.traverser()
         - self.revenir()
     """
-    timeout = 100.
+    timeout = 100
+    timeoutVC = 2
     cond = infem.ou2Semaphore(sem5,sem6,timeout)
     while cond == 2:
+        temps = time.time()
         self.log("En attente des voitures")
-        for i in range(4):
-            if infem.ou2Semaphore(sem5,sem3,timeout)==1:
+        cnt=0
+        nbVoiture = 0
+        while cnt < 4:
+            testDepart = infem.ou2Semaphore(sem5,sem3,timeoutVC)
+            self.log("Test départ : " + str(testDepart))
+            if testDepart == 1:#Fin du thread
                 return 0
+            elif testDepart == 3:#Fin du timer
+                self.log(str(time.time()-temps))
+                nbVoiture = cnt
+                cnt = 4
+            else:#Ajout d'une voiture au bac
+                cnt+=1
+        for i in range(nbVoiture):        
+            sem8.acquire()
         self.log("Eh ze bardi !")
         self.traverser()
-        for i in range(4):
+        for i in range(nbVoiture):
             sem2.release()
-        for i in range(4):
+        for i in range(nbVoiture):
             if infem.ou2Semaphore(sem5,sem4,timeout)==1:
                 return 0
         self.log("Eh za revient !")
         self.revenir()
-        for i in range(4):
+        for i in range(nbVoiture):
             sem1.release()
         sem6.release()
         cond = infem.ou2Semaphore(sem5,sem6,timeout)
@@ -54,8 +68,9 @@ def carRun(self):
         - self.embarquer()
         - self.debarquer()
     """
-    timeout = 100.
+    timeout = 100
     cond = infem.ou2Semaphore(sem5,sem7,timeout)
+    
     while cond == 2:
         self.avancer()
         self.log("Devant l'eau!")
@@ -63,6 +78,7 @@ def carRun(self):
             return 0
         else:
             self.embarquer()
+            sem8.release()
             sem3.release()
         if infem.ou2Semaphore(sem5,sem2,timeout)==1:
             return 0
