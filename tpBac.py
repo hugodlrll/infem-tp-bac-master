72d82d8269f78ac9336fcadce011d65cc6d32950
marrev <julien.depeyris@laposte.net>
2024-10-16 09:49:27 +0200

    Version B correcte
    Signed-off-by: marrev <julien.depeyris@laposte.net>

--- tpBac.py
+++ tpBac.py
@@ -18,47 +18,36 @@ sem4 = infem.Semaphore(0,"Retour")
 sem5 = infem.Semaphore(0,"Terminaison propre")
 sem6 = infem.Semaphore(1,"Thread Bateau")
 sem7 = infem.Semaphore(10,"Threads voiture")
-sem8 = infem.Semaphore(0, "Sécu embarquement")
+
 
 def ferryRun(self):
     """ fonction principale du bac. Les methodes principales sont:
         - self.traverser()
         - self.revenir()
     """
-    timeout = 100
-    timeoutVC = 2
-    cond = infem.ou2Semaphore(sem5,sem6,timeout)
+    cond = infem.ou2Semaphore(sem5,sem6)
     while cond == 2:
-        temps = time.time()
         self.log("En attente des voitures")
-        cnt=0
-        nbVoiture = 0
-        while cnt < 4:
-            testDepart = infem.ou2Semaphore(sem5,sem3,timeoutVC)
-            self.log("Test départ : " + str(testDepart))
-            if testDepart == 1:#Fin du thread
-                return 0
-            elif testDepart == 3:#Fin du timer
-                self.log(str(time.time()-temps))
-                nbVoiture = cnt
-                cnt = 4
-            else:#Ajout d'une voiture au bac
-                cnt+=1
-        for i in range(nbVoiture):        
-            sem8.acquire()
-        self.log("Eh ze bardi !")
-        self.traverser()
-        for i in range(nbVoiture):
-            sem2.release()
-        for i in range(nbVoiture):
-            if infem.ou2Semaphore(sem5,sem4,timeout)==1:
-                return 0
-        self.log("Eh za revient !")
-        self.revenir()
-        for i in range(nbVoiture):
-            sem1.release()
-        sem6.release()
-        cond = infem.ou2Semaphore(sem5,sem6,timeout)
+        nbVE = 0
+        while nbVE < 4 and cond == 2:
+            cond = infem.ou2Semaphore(sem5,sem3)
+            nbVE += 1
+        if cond == 2:
+            self.log("Eh ze bardi !")
+            self.traverser()
+            for i in range(4):
+                sem2.release()
+        nbVD = 0
+        while nbVD < 4 and cond == 2:
+            infem.ou2Semaphore(sem5,sem4)
+            nbVD += 1
+        if cond == 2:
+            self.log("Eh za revient !")
+            self.revenir()
+            for i in range(4):
+                sem1.release()
+            sem6.release()
+        cond = infem.ou2Semaphore(sem5,sem6)
 
     self.log("Fin de tâche")
 
@@ -68,32 +57,25 @@ def carRun(self):
         - self.embarquer()
         - self.debarquer()
     """
-    timeout = 100
-    cond = infem.ou2Semaphore(sem5,sem7,timeout)
-    
+    cond = cond = infem.ou2Semaphore(sem5,sem7)
     while cond == 2:
         self.avancer()
         self.log("Devant l'eau!")
-        if infem.ou2Semaphore(sem5,sem1,timeout)==1:
-            return 0
-        else:
+        if infem.ou2Semaphore(sem5,sem1)==2:
             self.embarquer()
-            sem8.release()
             sem3.release()
-        if infem.ou2Semaphore(sem5,sem2,timeout)==1:
-            return 0
-        else:
+        if infem.ou2Semaphore(sem5,sem2)==2:
             self.debarquer()
             sem4.release()
             sem7.release()
-        cond = infem.ou2Semaphore(sem5,sem7,timeout)
+        cond = infem.ou2Semaphore(sem5,sem7)
     
     self.log("Fin de tâche")
 
 def terminateCalled(self):
     """ fonction appelée lors d'un clic sur le bouton de terminaison """
     print("L'application doit se terminer correctement")
-    for i in range(11):
+    for i in range(32):
         sem5.release()
 
 #associe les fonctions aux methodes de classes (ne pas modifier)
