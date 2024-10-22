f2410ca743b7cf5396bad24fabd6cc0e5ede3c40
marrev <julien.depeyris@laposte.net>
2024-10-22 18:29:50 +0200

    Version C done
    Signed-off-by: marrev <julien.depeyris@laposte.net>

--- tpBac.py
+++ tpBac.py
@@ -31,31 +31,35 @@ def ferryRun(self):
         nbVE = 0
         t1 = time.time()
         duree = 0
-        while nbVE < 4 and cond == 2 and duree < 2:
+        while nbVE < 4 and cond == 2 and duree <= 2:
             duree = time.time() - t1
-            if duree < 2:
-                cond = infem.ou2Semaphore(sem5,sem3)
-                nbVE += 1
+            if duree <= 2:
+                cond = infem.ou2Semaphore(sem5,sem3,2-duree)
+                if cond == 2:
+                    nbVE += 1
+        if cond == 3:
+            for i in range(4-nbVE):
+                infem.ou2Semaphore(sem5,sem1)
         nbV = 0
-        while nbV < nbVE and cond == 2:
+        while nbV < nbVE and cond != 1:
             cond = infem.ou2Semaphore(sem5,sem8)
+            nbV += 1
         self.log("Eh ze bardi !")
         self.traverser()
-        if cond == 2:    
-            if duree < 2:
-                for i in range(nbVE):
-                    sem2.release()
-            nbVD = 0
-            while nbVD < 4 and cond == 2:
-                infem.ou2Semaphore(sem5,sem4)
-                nbVD += 1
+        cnt = nbVE
+        while cnt > 0:
+            sem2.release()
+            cnt -= 1
+        nbVD = 0
+        while nbVD < nbVE and cond != 1:
+            infem.ou2Semaphore(sem5,sem4)
+            nbVD += 1
         self.log("Eh za revient !")
         self.revenir()
-        for i in range(nbVE):
+        for i in range(4):
             sem1.release()
         sem6.release()
         cond = infem.ou2Semaphore(sem5,sem6)
-    #self.log("Fin de tâche")
 
 def carRun(self):
     """ fonction principale de chaque voiture. Les méthodes principales sont:
@@ -63,10 +67,9 @@ def carRun(self):
         - self.embarquer()
         - self.debarquer()
     """
-    cond = cond = infem.ou2Semaphore(sem5,sem7)
+    cond = infem.ou2Semaphore(sem5,sem7)
     while cond == 2:
         self.avancer()
-        #self.log("Devant l'eau!")
         if infem.ou2Semaphore(sem5,sem1)==2:
             self.embarquer()
             sem8.release()
@@ -74,10 +77,8 @@ def carRun(self):
         if infem.ou2Semaphore(sem5,sem2)==2:
             self.debarquer()
             sem4.release()
-            sem7.release()
+        sem7.release()
         cond = infem.ou2Semaphore(sem5,sem7)
-    
-    #self.log("Fin de tâche")
 
 def terminateCalled(self):
     """ fonction appelée lors d'un clic sur le bouton de terminaison """
